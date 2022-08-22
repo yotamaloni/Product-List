fd9df905ee5358b57779c0ca87864bba9c202261
Yotam <yotam.aloni1@gmail.com>
2022-08-22 10:58:28 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import { createApp, VueElement } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@/assets/style/global.scss'
-import Datepicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@/assets/style/global.scss";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.component('Datepicker', Datepicker);
-app.use(router)
-app.mount('#app')
+app.component("Datepicker", Datepicker);
+app.use(router);
+app.mount("#app");
